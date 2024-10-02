564c1f2daac98d423731864dc1aed6e807eff140
jiahn <jiahn@gmarket.com>
2024-10-02 22:37:18 +0900

    coding test

---
+++ myvenv/code/part1/ch2/02.py
@@ -0,0 +1,53 @@
+# https://www.acmicpc.net/problem/2512
+
+# num = int(input())
+# jibang = list(map(int, input().split()))
+# budget = int(input())
+#
+# left = 0
+# right = max(jibang)
+# answer = -1
+#
+# while (True):
+#     if left >= right:
+#         break
+#
+#     middle = (left + right) // 2
+#     sum = 0
+#
+#     for i in jibang:
+#         sum += min(middle, i)
+#
+#     if sum < budget:
+#         answer = middle
+#         left = middle + 1
+#     elif sum > budget:
+#         right = middle - 1
+#     else:
+#         answer = middle
+#
+# print(answer)
+
+num = int(input())
+jibang = list(map(int, input().split()))
+budget = int(input())
+
+left = 0
+right = max(jibang)
+answer = -1
+
+while left <= right:
+    sum = 0
+    middle = (left + right) // 2
+    for i in range(num):
+        sum += min(middle, jibang[i])
+
+    if sum <= budget:
+        answer = middle
+        left = middle + 1
+    else:
+        right = middle - 1
+
+print(answer)
+
+
---
+++ myvenv/code/part1/ch2/03.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/2343
+
+N, M = map(int, input().split())
+running_time = list(map(int, input().split()))
+
+left = max(running_time)
+right = sum(running_time)
+answer = -1
+
+while left <= right:
+    mid = (left + right) // 2
+    bluray_num = 1
+    remain = mid
+
+    for i in range(N):
+        if remain < running_time[i]:
+            bluray_num += 1
+            remain = mid
+
+        remain -= running_time[i]
+
+    if bluray_num <= M:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
